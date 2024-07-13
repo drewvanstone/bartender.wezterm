0b45b1b46f05f36da48ec4c285eb06cc1198aa43
Drew Flower <dnf@amazon.com>
2024-07-13 15:06:58 -0700

    Testig

--- plugin/init.lua
+++ plugin/init.lua
@@ -10,16 +10,17 @@ function M.apply_to_config(config, opts)
   config = config
 
   wezterm.log_info 'status plugin loaded'
-
-  wezterm.on("update-right-status", function(window, pane)
-      --local success, stdout, stderr = wezterm.run_child_process({"env", "KUBECONFIG="..pane:get_user_vars().KUBECONFIG, "kubectx", "-c"});
-
-      window:set_right_status(wezterm.format({
-        {Attribute={Underline="Double"}},
-        {Attribute={Intensity="Bold"}},
-        {Text="⎈ "}
-      }));
-  end);
 end
+
+
+wezterm.on("update-right-status", function(window, pane)
+    --local success, stdout, stderr = wezterm.run_child_process({"env", "KUBECONFIG="..pane:get_user_vars().KUBECONFIG, "kubectx", "-c"});
+
+    window:set_right_status(wezterm.format({
+      {Attribute={Underline="Double"}},
+      {Attribute={Intensity="Bold"}},
+      {Text="⎈ "}
+    }));
+end);
 
 return M
